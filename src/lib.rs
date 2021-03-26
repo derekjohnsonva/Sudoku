41bbd8e463a998a3ec036bc6cddcd67fcbaa1454
Derek Johnson <dej3tc@virginia.edu>
2021-03-26 16:27:01 -0400

    improvements to runtime

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,13 @@ use std::convert::TryInto;
 struct Point {
     x: i8,
     y: i8,
+    possibilities: Vec<i8>,
 }
 
-fn get_row_possibilities(puzzle: & [[i8; 9]; 9], p: &Point) -> [i8; 9] {
+fn get_row_possibilities(puzzle: & [[i8; 9]; 9], x: i8) -> [i8; 9] {
     let mut row_possiblities: [i8; 9] = [0; 9];
     for i in 0..9 {
-        let cell = puzzle[p.x as usize][i];
+        let cell = puzzle[x as usize][i];
         if cell != 0 {
             row_possiblities[cell as usize - 1] = 1;
         }
@@ -16,10 +17,10 @@ fn get_row_possibilities(puzzle: & [[i8; 9]; 9], p: &Point) -> [i8; 9] {
     return row_possiblities;
 }
 
-fn get_col_possibilities(puzzle: & [[i8; 9]; 9], p: &Point) -> [i8; 9] {
+fn get_col_possibilities(puzzle: & [[i8; 9]; 9], y: i8) -> [i8; 9] {
     let mut col_possiblities: [i8; 9] = [0; 9];
     for i in 0..9 {
-        let cell = puzzle[i][p.y as usize];
+        let cell = puzzle[i][y as usize];
         if cell != 0 {
             col_possiblities[cell as usize - 1] = 1;
         }
@@ -27,9 +28,9 @@ fn get_col_possibilities(puzzle: & [[i8; 9]; 9], p: &Point) -> [i8; 9] {
     return col_possiblities;
 }
 
-fn get_square_possibilities(puzzle: & [[i8; 9]; 9], p: &Point) -> [i8; 9] {
-    let x_ind: usize = (p.x / 3 * 3).try_into().unwrap();
-    let y_ind: usize = (p.y / 3 * 3).try_into().unwrap();
+fn get_square_possibilities(puzzle: & [[i8; 9]; 9], x: i8, y: i8) -> [i8; 9] {
+    let x_ind: usize = (x / 3 * 3).try_into().unwrap();
+    let y_ind: usize = (y / 3 * 3).try_into().unwrap();
     let mut square_possiblities: [i8; 9] = [0; 9];
     for x in 0..3 {
         for y in 0..3 {
@@ -42,15 +43,15 @@ fn get_square_possibilities(puzzle: & [[i8; 9]; 9], p: &Point) -> [i8; 9] {
     return square_possiblities;
 }
 
-fn get_valid_numbers(puzzle: & [[i8; 9]; 9], p: &Point) -> Vec<i8> {
+fn get_valid_numbers(puzzle: & [[i8; 9]; 9], x: i8, y: i8) -> Vec<i8> {
     // Loop over the row the point is in
     // if a val is in possibilities, update 
     // the array with the val 1
-    let row_possiblities: [i8; 9] = get_row_possibilities(puzzle, p);
+    let row_possiblities: [i8; 9] = get_row_possibilities(puzzle, x);
     
-    let col_possiblities: [i8; 9] = get_col_possibilities(puzzle, p);
+    let col_possiblities: [i8; 9] = get_col_possibilities(puzzle, y);
 
-    let square_possiblities: [i8; 9] = get_square_possibilities(puzzle, p);
+    let square_possiblities: [i8; 9] = get_square_possibilities(puzzle, x, y);
 
     let mut temp;
     let mut output: Vec<i8> = Vec::new();
@@ -64,10 +65,11 @@ fn get_valid_numbers(puzzle: & [[i8; 9]; 9], p: &Point) -> Vec<i8> {
     return output;
 }
 // 
-fn solve(puzzle: &mut [[i8; 9]; 9], unfilled: &mut Vec<Point>) -> bool {
+fn solve(puzzle: &mut [[i8; 9]; 9]) -> bool {
     // Check to see if there are any empty spaces
     // If not, return true. The puzzle is solved!
     // If there are empty spaces, get the best one
+    let mut unfilled = get_unfilled(&puzzle);
     let point: Point;
     match unfilled.pop() {
         None => return true,
@@ -75,17 +77,17 @@ fn solve(puzzle: &mut [[i8; 9]; 9], unfilled: &mut Vec<Point>) -> bool {
             point = p;
         }
     }
-    // Determine which numbers are valid possiblities 
-    // for filling in that space
-    let valid: Vec<i8> = get_valid_numbers(puzzle, &point);
-    // Iterate over the valid possibilites
+
+    // Iterate over the valid possibilities
     // Plug each one in to the puzzle and recurse on that 
     // new puzzle. 
     // If we get a true return from our recursive call, return true
     // Else, set the possibility back to 0
-    for number in valid.iter() {
+    // println!("trying point {}, {}", point.x, point.y);
+
+    for number in point.possibilities.iter() {
         puzzle[point.x as usize][point.y as usize] = *number;
-        if solve(puzzle, unfilled){
+        if solve(puzzle){
             return true;
         }
         puzzle[point.x as usize][point.y as usize] = 0;
@@ -96,10 +98,8 @@ fn solve(puzzle: &mut [[i8; 9]; 9], unfilled: &mut Vec<Point>) -> bool {
     return false
 }
 
-pub fn parse_and_solve(content: &str) -> [[i8; 9]; 9] {
-    // parse the string into a two dimentional array
+fn parse_puzzle(content: &str) -> [[i8; 9]; 9] {
     let mut puzzle: [[i8; 9]; 9] = [[0; 9]; 9];
-    let mut unfilled: Vec<Point> = Vec::new();
 
     let radix: u32 = 10;
     let mut count = 0;
@@ -114,13 +114,39 @@ pub fn parse_and_solve(content: &str) -> [[i8; 9]; 9] {
         let y = count % 9;
         count += 1;
         puzzle[x][y] = val as i8;
-        if val == 0 {
-            let new_point: Point = Point{x: x as i8, y: y as i8};
-            unfilled.push(new_point);
+    }
+    return puzzle;
+}
+
+fn get_unfilled(puzzle: & [[i8; 9]; 9]) -> Vec<Point> {
+    let mut output: Vec<Point> = Vec::new();
+
+    for x in 0..9 {
+        for y in 0..9 {
+            let cell = puzzle[x][y];
+            if cell == 0 {
+                let possibilities: Vec<i8> = get_valid_numbers(puzzle, x as i8, y as i8);
+                let point: Point = Point{x: x as i8, y: y as i8, possibilities};
+                output.push(point);
+            }
         }
     }
 
-    solve(&mut puzzle, &mut unfilled);
+    // reorder the vector so that the points 
+    // with the least possibilities are popped first
+    output.sort_by_cached_key(|p| p.possibilities.len());
+    output.reverse();
+    // for o in output.iter() {
+    //     println!("possibilities for point {}, {} = {:?}", o.x, o.y, o.possibilities);
+    // }
+    return output;
+}
+
+pub fn parse_and_solve(content: &str) -> [[i8; 9]; 9] {
+    // parse the string into a two dimentional array
+    let mut puzzle: [[i8; 9]; 9] = parse_puzzle(&content);
+    // let mut unfilled: Vec<Point> = get_unfilled(&puzzle);
+    solve(&mut puzzle);
     return puzzle
 }
 
@@ -140,7 +166,6 @@ pub fn puzzle_to_string(puzzle: &[[i8; 9]; 9]) -> String {
         }
         output.push_str("\n");
     }
-    output.push_str("\n");
     return output;
 }
 
@@ -148,20 +173,9 @@ pub fn print_to_output(content: &str, mut writer: impl std::io::Write) {
     writeln!(writer, "{}", content);
 }
 
-#[cfg(test)]
-fn get_unfilled(puzzle: &mut [[i8; 9]; 9]) -> Vec<Point> {
-    let mut output: Vec<Point> = Vec::new();
-    for x in 0..9 {
-        for y in 0..9 {
-            let cell = puzzle[x][y];
-            if cell == 0 {
-                let point: Point = Point{x: x as i8, y: y as i8};
-                output.push(point);
-            }
-        }
-    }
-    return output;
-}
+
+
+
 #[cfg(test)]
 fn test_board() -> [[i8; 9]; 9] {
     [
@@ -213,18 +227,16 @@ fn test_board_str() -> &'static str {
 #[test]
 fn test_get_valid_numbers() {
     let mut board = almost_solved_test_board();
-    let p = Point{x: 0, y: 3};
     let expected = vec![2];
-    let actual = get_valid_numbers(&mut board, &p);
+    let actual = get_valid_numbers(&mut board, 0, 3);
     assert_eq!(expected, actual)
 }
 
 #[test]
 fn test_get_valid_numbers_2() {
     let mut board = test_board();
-    let p = Point{x: 1, y: 1};
     let expected = vec![2, 6, 9];
-    let actual = get_valid_numbers(&mut board, &p);
+    let actual = get_valid_numbers(&mut board, 1, 1);
     assert_eq!(expected, actual)
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use structopt::StructOpt;
+use std::time::{Duration, Instant};
 
 
 #[derive(StructOpt)]
@@ -16,10 +17,23 @@ fn main() -> Result<(),CustomError> {
     .map_err(|err| CustomError(format!("Error reading `{}`: {}", args.input.display(), err)))?;
 
     // TODO: Do this with threads
+    // total time in miliseconds
+    let mut total_time: u128 = 0;
+    let mut count:u32 = 0;
     for line in content.lines() {
+        let now = Instant::now();
         let result = sudoku::parse_and_solve(line);
+        let elapsed = now.elapsed().as_millis();
+
+        let elapsed_str: String = format!("miliseconds to solve puzzle = {}\n", elapsed);
+        total_time += elapsed;
+        count += 1;
         let result_str: String = sudoku::puzzle_to_string(&result);
         sudoku::print_to_output(&result_str, &mut std::io::stdout());
+        sudoku::print_to_output(&elapsed_str, &mut std::io::stdout());
     }
+    let average = total_time/(count as u128);
+    let average_str: String = format!("{} runs took an average of {} ms", count, average);
+    sudoku::print_to_output(&average_str, &mut std::io::stdout());
     Ok(())
 }
